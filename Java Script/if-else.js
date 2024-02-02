912131f29314adf66da08811a96baedf6afc1191
Gowtham Reddy <gowthamreddy246@gmail.com>
2024-02-02 11:23:02 -0600

    if else statements

---
+++ Java Script/if-else.js
@@ -0,0 +1,11 @@
+prompt("What is your name ? ");
+prompt("What is their name ? ");
+
+var number = Math.random() * 100 ;
+number = Math.floor(number) + 1;
+
+if(number > 70) {
+    alert("Your love Score is " + number + "%," + " You are made for each other.");
+} else {
+    alert("Your love Score is " + number + "%." );
+}
--- Java Script/numberGenerator.js
+++ Java Script/numberGenerator.js
@@ -1,7 +1,1 @@
-prompt("What is your name ? ");
-prompt("What is their name ? ");
-
-var number = Math.random() * 100 ;
-number = Math.floor(number) + 1;
-console.log(number);
-alert("Your Love Score is " + number + "%");
+6
