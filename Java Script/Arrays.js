c4bb7d0d379ff9f3bae32a7922a1c308d865084a
Gowtham Reddy <gowthamreddy246@gmail.com>
2024-02-02 14:21:24 -0600

    Basic Arrays Concept

---
+++ Java Script/Arrays.js
@@ -0,0 +1,7 @@
+var guestList = ["Gowtham", "Chandu", "Ganesh", "Rudh", "Yashwanth", "Madhav"];
+var name = prompt("Please Enter Your Name ");
+if (guestList.includes(name)){
+    alert("Welcome!");
+}else {
+    alert("Sorry may be next time.");
+}
