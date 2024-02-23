5822e0634da3f0dad3b767ea323004c181c85524
Gowtham Reddy <gowthamreddy246@gmail.com>
2024-02-23 10:33:39 -0600

    first react code

--- React/index.js
+++ React/index.js
@@ -1,4 +1,10 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 
-ReactDOM.render(<h1>Hello, world!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
