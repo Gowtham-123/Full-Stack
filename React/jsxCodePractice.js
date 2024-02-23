6e0541289495e1bf20579a8f7386011143740263
Gowtham Reddy <gowthamreddy246@gmail.com>
2024-02-23 10:45:10 -0600

    jsx code practice

---
+++ React/jsxCodePractice.js
@@ -0,0 +1,14 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Food</h1>
+    <ul>
+      <li>Chicken</li>
+      <li>Mutton</li>
+      <li>Shrimp</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
