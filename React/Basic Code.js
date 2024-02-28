f25bebdbc2a5cc52f7aa05d207b3a6cee6b7d39d
Gowtham Reddy <gowthamreddy246@gmail.com>
2024-02-27 19:48:06 -0600

    Basic React Code

--- React/Basic Code.js
+++ React/Basic Code.js
@@ -1,13 +1,24 @@
-<html>
-  <body>
-    <h1>Hello World</h1>
-    <script
-      crossorigin
-      src="https://unpkg.com/react@18/umd/react.development.js"
-    ></script>
-    <script
-      crossorigin
-      src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"
-    ></script>
-  </body>
-</html>;
+<<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>React</title>
+  </head>
+
+  <body></body>
+  <div id="root"></div>
+  <script
+    crossorigin
+    src="https://unpkg.com/react@18/umd/react.development.js"
+  ></script>
+  <script
+    crossorigin
+    src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"
+  ></script>
+  <script>
+    const heading = React.createElement("h1", {}, "Namaste React!");
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(heading);
+  </script>
+</html>
